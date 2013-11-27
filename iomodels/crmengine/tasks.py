119d2fd40b6745f1b4b3d26ba2e33f79c2111afd
tedj <tedj.meabiou@gmail.com>
2013-11-27 18:31:52 +0100

    fixing tasks.get api

--- iomodels/crmengine/tasks.py
+++ iomodels/crmengine/tasks.py
@@ -2,7 +2,7 @@ from google.appengine.ext import ndb
 from google.appengine.api import search 
 from endpoints_proto_datastore.ndb import EndpointsModel
 from iomodels.crmengine.notes import Topic
-from model import User
+from model import Userinfo
 import pprint
 
 import model
@@ -10,19 +10,19 @@ import model
 class Task(EndpointsModel):
     _message_fields_schema = ('id','owner','created_at','updated_at','title','due','status','completed_by','comments','about_kind','about_item','organization')
 
-    author = ndb.StructuredProperty(User)
+    author = ndb.StructuredProperty(Userinfo)
     # Sharing fields
     owner = ndb.StringProperty()
     collaborators_list = ndb.StructuredProperty(model.Userinfo,repeated=True)
     collaborators_ids = ndb.StringProperty(repeated=True)
     created_at = ndb.DateTimeProperty(auto_now_add=True)
     updated_at = ndb.DateTimeProperty(auto_now=True)
-    title = ndb.StringProperty(required=True)
+    title = ndb.StringProperty()
     due = ndb.DateTimeProperty()
-    status = ndb.StringProperty(default='open')
-    completed_by = ndb.StructuredProperty(User)
+    status = ndb.StringProperty()
+    completed_by = ndb.StructuredProperty(Userinfo)
     # number of comments in this topic
-    comments = ndb.IntegerProperty(default=0)
+    comments = ndb.IntegerProperty()
     # A Topic is attached to an object for example Account or Opportunity..
     about_kind = ndb.StringProperty()
     about_item = ndb.StringProperty()
@@ -62,10 +62,7 @@ class Task(EndpointsModel):
             search.TextField(name='collaborators', value = collaborators ),
             search.TextField(name='title', value = empty_string(self.title) ),
             search.TextField(name='status', value = empty_string(self.status)),
-            search.DateField(name='created_at', value = self.created_at),
-            search.DateField(name='updated_at', value = self.updated_at),
-            #search.DateField(name='due', value = self.due),
-            search.NumberField(name='comments', value = self.comments),
+            search.DateField(name='due', value = self.due),
             search.TextField(name='about_kind', value = empty_string(self.about_kind)),
             search.TextField(name='about_item', value = empty_string(self.about_item)),
            ])
