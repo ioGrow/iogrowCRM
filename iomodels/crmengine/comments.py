2dcc590bfeb3de2f2c9135513f1d7009002b59f4
hakim <h.karriche@werework.com>
2013-12-05 15:14:09 +0100

    for Pull

--- iomodels/crmengine/comments.py
+++ iomodels/crmengine/comments.py
@@ -3,14 +3,19 @@ from google.appengine.api import search
 from endpoints_proto_datastore.ndb import EndpointsModel
 from model import User
 from model import Userinfo
+import model
 import pprint
 
 class Comment(EndpointsModel):
 	_message_fields_schema = ('content','discussion','last_updater','updated_at','author')
 	author= ndb.StructuredProperty(Userinfo)
 	last_updater = ndb.StructuredProperty(User)
+	collaborators_list = ndb.StructuredProperty(model.Userinfo,repeated=True)
+	collaborators_ids = ndb.StringProperty(repeated=True)
 	created_at = ndb.DateTimeProperty(auto_now_add=True)
 	updated_at = ndb.DateTimeProperty(auto_now=True)
 	content = ndb.StringProperty(required=True)
 	discussion = ndb.KeyProperty()
+	access = ndb.StringProperty()
 	organization = ndb.KeyProperty()
+
