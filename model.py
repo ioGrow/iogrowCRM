1ac3d2c5f0610ed73cf8d08ad5c80586ee2d4818
hakim <h.karriche@werework.com>
2013-12-17 17:11:22 +0100

    pull

--- model.py
+++ model.py
@@ -43,11 +43,11 @@ STANDARD_APPS = [{'name': 'sales', 'label': 'Sales', 'url':'/#/accounts/'},#{'na
 STANDARD_OBJECTS = ['Account','Contact','Opportunity','Lead','Case','Campaign']
 ADMIN_TABS = [{'name': 'Users','label': 'Users','url':'/#/admin/users'},{'name': 'Groups','label': 'Groups','url':'/#/admin/groups'},{'name': 'Settings','label': 'Settings','url':'/#/admin/settings'}]
 ADMIN_APP = {'name': 'admin', 'label': 'Admin Console', 'url':'/#/admin/users'}
-Default_Opp_Stages = [{'name':'Open','probability':'5%'},{'name':'Open','probability':'100%'}]
-Default_Case_Status =[{'status':'Open'},{'status':'Close'}]
-Default_Lead_Status =[{'status':'Open'},{'status':'Close'}]
+Default_Opp_Stages = [{'name':'incoming','probability':'5'},{'name':'qualified','probability':'10'},{'name':'Need Analysis','probability':'40'},{'name':'Negociating','probability':'80'},{'name':'Clos won','probability':'100'},{'name':'Clos lost','probability':'0'}]
+Default_Case_Status =[{'status':'New'},{'status':'Working'},{'status':'Escalated'}]
+Default_Lead_Status =[{'status':'New'},{'status':'Working'},{'status':'Unqualified'},{'status':'Closed converted'}]
 
-
+ 
 
 # Models for Appcfg
 # The Object class will be useful to manage Default Sharing setting to each organization
