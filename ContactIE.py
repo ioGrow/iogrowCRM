0960e17d5b14fe00fdb910914ab0f11838ea9217
Assem Chelli <assem.ch@gmail.com>
2014-01-19 17:21:01 +0100

    importing/exporting contacts from/into  CSV

--- ContactIE.py
+++ ContactIE.py
@@ -4,9 +4,12 @@ text files
 
 dependencies: pyvcf, xlrd
 
+
+Supported types:
+implemented => iogrow_csv, txt
+unimplemented =>
+
 """
-
-
 
 import vcf,  xlrd, csv
 
@@ -37,33 +40,102 @@ class Contact:
         self.url = url
         self.address = address
 
+    def __repr__(self):
+        return self.given_name + " " + self.family_name + "(" + self.display_name + ")"
+    
+    def __str__(self):
+        string = "[Contact : %s ]" % self.display_name + "\n"
+        string += "Given Name: " + self.given_name + "\n"
+        string += "Family Name: " + self.family_name + "\n"
+        string += "Display Name: " + self.display_name + "\n"
+        string += "Title: " + self.title  + "\n"
 
-    def importer(self, type, file):
+        string += "Emails: " + ",".join(self.emails) + "\n"
+        string += "Organization: " + self.organization + "\n"
+        string += "Phone: " + self.given_name + "\n"
+        string += "Fax: " + self.fax + "\n"
+        string += "Fax: " + self.fax + "\n"
+        string += "Url: " + self.url + "\n"
+        string += "Address: " + self.address + "\n"
+
+        return string
+
+    def importer(self, file_type, file_path):
         """
         types: vcf, txt, exls, iogrow_csv, evolution_csv, mozilla_csv, outlook_csv 
         """
-        f = open(file, "r")
-        if type == "csv":
-            pass
-    
-    def exporter(self, type, file):
+        f = open(file_path, "rb")
+        if file_type == "iogrow_csv":
+            csvreader = csv.reader(f, delimiter=';')
+            for row in csvreader:
+                self.given_name = row[0]
+                self.family_name = row[1]
+                self.display_name = row[2]
+                self.title = row[3]
+                self.emails = row[4]
+                self.organization = row[5]
+                self.phone = row[6]
+                self.fax = row[7]
+                self.url = row[8]
+                self.address = row[9]
+        
+        f.close()
+
+    def exporter(self, file_type, file_path):
         """
-        types: vcf, txt, iogrow_csv, evolution_csv, mozilla_csv, outlook_csv , pdf
+        types: vcf, txt, exls,  iogrow_csv, evolution_csv, mozilla_csv, outlook_csv , pdf
         """
-        f = open(file, "w")
-        if type == "iogrow_csv":
-            f.writelines([
-                          "given_name;family_name;display_name;title;email;organization;phone;fax;url;address"
-                          "%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;" % (
-                                                              self.given_name,
-                                                              self.family_name,
-                                                              self.display_name,
-                                                              self.title,
-                                                              self.emails[0],
-                                                              self.organization,
-                                                              self.phone,
-                                                              self.fax,
-                                                              self.url,
-                                                              self.address
-                                                              )
-                          ])
+        f = open(file_path, "w")
+        if file_type == "iogrow_csv":
+            csvwriter = csv.writer(f, delimiter=';')
+            csvwriter.writerow([
+                                "given_name",
+                                "family_name",
+                                "display_name",
+                                "title",
+                                "email",
+                                "organization",
+                                "phone",
+                                "fax",
+                                "url",
+                                "address"
+                                ]
+                                )
+            csvwriter.writerow(
+                               [
+                                  self.given_name,
+                                  self.family_name,
+                                  self.display_name,
+                                  self.title,
+                                  self.emails[0],
+                                  self.organization,
+                                  self.phone,
+                                  self.fax,
+                                  self.url,
+                                  self.address
+                                ])
+        elif file_type == "txt":
+            f.write(str(self))
+
+        f.close()
+
+
+if __name__ ==  "__main__":
+    contact = Contact(
+                         given_name = 'Assem',
+                         family_name= 'Chelli',
+                         display_name = 'BigO',
+                         title = "Engineer",
+                         emails = ["assem.ch@gmail.com"],
+                         organization= "iogrow",
+                         phone = "00213794243270",
+                         fax = "00213794243270",
+                         url = "00213794243270",
+                         address = "Cite 23 logs bt 4 n6 Taher"
+                          )
+    print repr(contact)
+    print str(contact)
+    contact.exporter("iogrow_csv","contact.csv")
+    contact2 = Contact()
+    contact2.importer("iogrow_csv","contact.csv")
+    print str(contact)
