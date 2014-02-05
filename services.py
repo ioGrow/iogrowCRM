0758f2482ba4d99b34da61e9141010ea3860324a
Tedj Meabiou <tedj.meabiou@gmail.com>
2014-02-05 21:41:16 +0100

    edges.insert api

--- services.py
+++ services.py
@@ -1,11 +1,3 @@
-# If you have not yet seen the source in basic/main.py, please take a look.
-
-# In this sample we add an additional method MyModelGet which allows a specific
-# entity to be retrieved.
-
 import endpoints
 import ioendpoints 
-
-
-
-application = endpoints.api_server([ioendpoints.CrmEngineApi,ioendpoints.LiveApi], restricted=False)
+application = endpoints.api_server([ioendpoints.CrmEngineApi,ioendpoints.EndGrow], restricted=False)
