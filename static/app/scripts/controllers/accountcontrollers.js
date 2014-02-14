52de0effde32eab7c25bb88f369e19f288985afe
hakim <h.karriche@werework.com>
2014-02-15 00:14:42 +0100

    Add Tag record and display list tag on account list

--- static/app/scripts/controllers/accountcontrollers.js
+++ static/app/scripts/controllers/accountcontrollers.js
@@ -1,5 +1,5 @@
-app.controller('AccountListCtrl', ['$scope','Auth','Account',
-    function($scope,Auth,Account) {
+app.controller('AccountListCtrl', ['$scope','Auth','Account','Tag',
+    function($scope,Auth,Account,Tag) {
      $("ul.page-sidebar-menu li").removeClass("active");
      $("#id_Accounts").addClass("active");
      document.title = "Accounts: Home";
@@ -22,6 +22,7 @@ app.controller('AccountListCtrl', ['$scope','Auth','Account',
           var params = { 'order': $scope.order,
                         'limit':6}
           Account.list($scope,params);
+          Tag.list($scope,{});
      };
      // We need to call this to refresh token when user credentials are invalid
      $scope.refreshToken = function() {
@@ -136,10 +137,235 @@ app.controller('AccountListCtrl', ['$scope','Auth','Account',
         Account.list($scope,params);
      };
 
+/***********************************************
+      HKA 14.02.2014  tags 
+***************************************************************************************/
+$scope.listTags=function(){
+      Tag.list($scope,{});
+     }
+$scope.addNewtag = function(tag){
+       var params = {   
+                          'name': tag.name,
+                          'color':$('#tag-col-pick').val()
+                      }  ;
+       Tag.insert($scope,params);
+        $scope.tag.name='';
+        Tag.list($scope,{});
+        
+     }
+$scope.updateTag = function(tag){
+            params ={ 'id':tag.id,
+                      'title': tag.name,
+                      'status':tag.color
+            };
+      Tag.patch($scope,params);
+  };
+$scope.selectTag= function(tag,index,$event){
+      if(!$scope.manage_tags){
+         var element=$($event.target);
+         if(element.prop("tagName")!='LI'){
+              element=element.parent();
+              element=element.parent();
+         }
+         var text=element.find(".with-color");
+         if($scope.selected_tags.indexOf(tag) == -1){
+            $scope.selected_tags.push(tag);
+            element.css('background-color', tag.color+'!important');
+            text.css('color',$scope.idealTextColor(tag.color));
+
+         }else{
+            element.css('background-color','#ffffff !important');
+            $scope.selected_tags.splice($scope.selected_tags.indexOf(tag),1);
+             text.css('color','#000000');
+         }
+         console.log('Taaaaaaaaaggggggssss');
+         console.log($scope.selected_tags);
+         $scope.filterByTags($scope.selected_tags);
+
+      }
+
+    };
+  $scope.filterByTags = function(selected_tags){
+         var tags = [];
+         angular.forEach(selected_tags, function(tag){
+            tags.push(tag.entityKey);
+         });
+         var params = {
+          'tags': tags
+         }
+         Task.list($scope,params);
+
+  };
+
+  var handleColorPicker = function () {
+          if (!jQuery().colorpicker) {
+              return;
+              console.log('errooooooooooooooor');
+              console.log("working******************************");
+          }
+          $('.colorpicker-default').colorpicker({
+              format: 'hex'
+          });
+      }
+      handleColorPicker();
+      console.log('heeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeer');
+      console.log($('#addMemberToTask').children());
+      $('#addMemberToTask > *').on('click', null, function(e) {
+            e.stopPropagation();
+        });
+      $scope.idealTextColor=function(bgColor){
+        var nThreshold = 105;
+         var components = getRGBComponents(bgColor);
+         var bgDelta = (components.R * 0.299) + (components.G * 0.587) + (components.B * 0.114);
+
+         return ((255 - bgDelta) < nThreshold) ? "#000000" : "#ffffff";  
+      }
+      function getRGBComponents(color) {       
+
+          var r = color.substring(1, 3);
+          var g = color.substring(3, 5);
+          var b = color.substring(5, 7);
+
+          return {
+             R: parseInt(r, 16),
+             G: parseInt(g, 16),
+             B: parseInt(b, 16)
+          };
+      };
+
+      $scope.dragTag=function(tag){
+        $scope.draggedTag=tag;
+      }
+      $scope.dropTag=function(task){
+        var items = [];
+        var edge = {
+              'start_node': task.entityKey,
+              'end_node': $scope.draggedTag.entityKey,
+              'kind':'tags',
+              'inverse_edge': 'tagged_on'
+        };
+        items.push(edge);
+        params = {
+          'items': items
+        }
+        Edge.insert($scope,params);
+        $scope.draggedTag=null;
+      }
+
+
+
+
      // Google+ Authentication 
      Auth.init($scope);
 
 }]);
+app.directive('ngBlur', ['$parse', function($parse) {
+  return function(scope, element, attr) {
+    var fn = $parse(attr['ngBlur']);
+    element.bind('blur', function(event) {
+      scope.$apply(function() {
+        fn(scope, {$event:event});
+      });
+    });
+  }
+}]);
+app.directive('ngDrag', ['$parse', function($parse) {
+  return function(scope, element, attr) {
+    var fn = $parse(attr['ngDrag']);
+    element.bind('drag', function(event) {
+      scope.$apply(function() {
+        fn(scope, {$event:event});
+      });
+    });
+  }
+}]);
+app.directive('ngDrop', ['$parse', function($parse) {
+  return function(scope, element, attr) {
+    var fn = $parse(attr['ngDrop']);
+    element.bind('drop', function(event) {
+      scope.$apply(function() {
+        fn(scope, {$event:event});
+      });
+    });
+  }
+}]);
+app.directive('draggable', function() {
+   return function(scope, element) {
+        // this gives us the native JS object
+        var el = element[0];
+
+        el.draggable = true;
+
+        el.addEventListener(
+            'dragstart',
+            function(e) {
+                e.dataTransfer.effectAllowed = 'move';
+                e.dataTransfer.setData('Text', this.id);
+                this.classList.add('drag');
+                return false;
+            },
+            false
+        );
+
+        el.addEventListener(
+            'dragend',
+            function(e) {
+                this.classList.remove('drag');
+                //alert('end of draggable');
+                return false;
+            },
+            false
+        );
+        el.addEventListener(
+            'drop',
+            function(e) {
+                // Stops some browsers from redirecting.
+                if (e.stopPropagation) e.stopPropagation();
+
+                this.classList.remove('over');
+
+                //var item = document.getElementById(e.dataTransfer.getData('Text'));
+                //this.appendChild(item);
+
+                return false;
+            },
+            false
+        );
+    }
+});
+app.directive('droppable', function() {
+    return function(scope, element) {
+        var el = element[0];
+        el.addEventListener(
+            'dragover',
+            function(e) {
+                e.dataTransfer.dropEffect = 'move';
+                // allows us to drop
+                if (e.preventDefault) e.preventDefault();
+                this.classList.add('over');
+                return false;
+            },
+            false
+        );
+        el.addEventListener(
+            'dragenter',
+            function(e) {
+                this.classList.add('over');
+                return false;
+            },
+            false
+        );
+
+        el.addEventListener(
+            'dragleave',
+            function(e) {
+                this.classList.remove('over');
+                return false;
+            },
+            false
+        );
+    }
+});
 app.controller('AccountShowCtrl', ['$scope','$filter', '$route','Auth','Account','Contact','Case','Opportunity', 'Topic','Note','Task','Event','Permission','User','Attachement','Email','Need','Opportunitystage','Casestatus','Map','InfoNode',
    function($scope,$filter,$route,Auth,Account,Contact,Case,Opportunity,Topic,Note,Task,Event,Permission,User,Attachement,Email,Need,Opportunitystage,Casestatus,Map,InfoNode) {
        $("ul.page-sidebar-menu li").removeClass("active");
