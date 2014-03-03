55250939bb066ffb27e2af5a992a676b853772df
Tedj Meabiou <tedj.meabiou@gmail.com>
2014-03-03 18:43:26 +0100

    Adding events.list_by_parent method

--- iomodels/crmengine/events.py
+++ iomodels/crmengine/events.py
@@ -1,12 +1,58 @@
 from google.appengine.ext import ndb
+from google.appengine.datastore.datastore_query import Cursor
 from google.appengine.api import search 
+from protorpc import messages
 from endpoints_proto_datastore.ndb import EndpointsModel
-from iomodels.crmengine.notes import Topic
+from iomodels.crmengine.notes import Topic, AuthorSchema,DiscussionAboutSchema
 from model import Userinfo
+from iograph import Edge
 import pprint
 
 import model
 
+# The message class that defines the EntityKey schema
+class EntityKeyRequest(messages.Message):
+    entityKey = messages.StringField(1)
+
+ # The message class that defines the ListRequest schema
+class ListRequest(messages.Message):
+    limit = messages.IntegerField(1)
+    pageToken = messages.StringField(2)
+
+class EventSchema(messages.Message):
+    id = messages.StringField(1)
+    entityKey = messages.StringField(2)
+    title = messages.StringField(3)
+    starts_at = messages.StringField(4)
+    ends_at = messages.StringField(5)
+    where = messages.StringField(6)
+    about = messages.MessageField(DiscussionAboutSchema,7)
+    created_at = messages.StringField(8)
+    updated_at = messages.StringField(9)
+
+class EventInsertRequest(messages.Message):
+    about = messages.StringField(1)
+    title = messages.StringField(2,required=True)
+    due = messages.StringField(3)
+    reminder = messages.StringField(4)
+    status = messages.StringField(5)
+    assignees = messages.MessageField(EntityKeyRequest,6, repeated = True)
+    tags = messages.MessageField(EntityKeyRequest,7, repeated = True)
+
+class EventListRequest(messages.Message):
+    limit = messages.IntegerField(1)
+    pageToken = messages.StringField(2)
+    order = messages.StringField(3)
+    status = messages.StringField(4)
+    tags = messages.StringField(5,repeated = True)
+    owner = messages.StringField(6)
+    assignee = messages.BooleanField(7)
+    about = messages.StringField(8)
+    urgent = messages.BooleanField(9)
+
+class EventListResponse(messages.Message):
+    items = messages.MessageField(EventSchema, 1, repeated=True)
+    nextPageToken = messages.StringField(2)
 
 class Event(EndpointsModel):
 
@@ -78,4 +124,36 @@ class Event(EndpointsModel):
            ])
         my_index = search.Index(name="GlobalIndex")
         my_index.put(my_document)
+
+    @classmethod
+    def list_by_parent(cls,parent_key,request):
+        date_time_to_string = lambda x: x.strftime("%Y-%m-%dT%H:%M:00.000") if x else ""
+        event_list = []
+        event_edge_list = Edge.list(
+                                start_node = parent_key,
+                                kind = 'events',
+                                limit=request.events.limit,
+                                pageToken=request.events.pageToken
+                                )
+        for edge in event_edge_list['items']:
+            event = edge.end_node.get()
+            event_schema = EventSchema(
+                                    id = str( event.key.id() ),
+                                    entityKey = event.key.urlsafe(),
+                                    title = event.title,
+                                    starts_at = event.starts_at.isoformat(),
+                                    ends_at = event.ends_at.isoformat(),
+                                    where = event.where,
+                                    created_at = event.created_at.isoformat(),
+                                    updated_at = event.updated_at.isoformat()
+                                )
+            event_list.append(event_schema)
+        if event_edge_list['next_curs'] and event_edge_list['more']:
+            event_next_curs = event_edge_list['next_curs'].urlsafe()
+        else:
+            event_next_curs = None
+        return EventListResponse(
+                                items = event_list,
+                                nextPageToken = event_next_curs
+                                )
   
