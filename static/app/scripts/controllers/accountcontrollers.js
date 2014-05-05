bb7bb4a0faf3407ea8bc4ca75c9e5095ec9ce724
hakim <h.karriche@werework.com>
2014-05-05 16:23:25 +0100

    inline edit phone, email on account show
    Former-commit-id: e04428ba6c075189230063dc6f533e4667ffe097

--- static/app/scripts/controllers/accountcontrollers.js
+++ static/app/scripts/controllers/accountcontrollers.js
@@ -1592,7 +1592,7 @@ $scope.deleteaccount = function(){
     };
 //HKA 12.03.2014 Edit infonode
 $scope.edit_email=function(email){
-  console.log('----------thanks----------');
+
         $scope.edited_email=email;
      };
 
@@ -1602,7 +1602,25 @@ $scope.editTag=function(tag){
 $scope.doneEditTag=function(tag){
         $scope.edited_tag=null;
         $scope.updateTag(tag);
-     }
+     };
+
+     $scope.initObject=function(obj){
+          for (var key in obj) {
+                obj[key]=null;
+              }
+      }
+
+          $scope.pushElement=function(elem,arr){
+          if (arr.indexOf(elem) == -1) {
+              var copyOfElement = angular.copy(elem);
+              arr.push(copyOfElement);
+              console.log(elem);
+              $scope.initObject(elem);
+
+          }else{
+            alert("item already exit");
+          }
+      }
 
 
      // Google+ Authentication 
