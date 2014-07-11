93e6439b9722f10b2e536cd991e8474ca7a96818
Tedj Meabiou <tedj.meabiou@gmail.com>
2014-07-11 17:52:02 +0100

    optimize google+ sign-in process fix #653
    Former-commit-id: 0531d54e88c8b37d0a058963fd12f6f14014b24d [formerly 5572b606dd335cef6606f6b038ea23c8182f48a7] [formerly 369aae15162827b9397f02fb83e199d24e7c8ae3 [formerly fff6d75d7d5807ea511ff4efa6bc947103c5c6b6]]
    Former-commit-id: 03dd32e10f46e2c9c7f051be1e192a88e5b89a75 [formerly 431fd415fb3d3dd3a7cb8c611d31ad40f8d112ec]
    Former-commit-id: 6121fadc25bc0b026828ad36c4fa68ea9013a8e3

--- static/app/scripts/services/authservices.js
+++ static/app/scripts/services/authservices.js
@@ -15,29 +15,32 @@ accountservices.factory('Auth', function($http) {
       }
       return false;
   }
-  Auth.init = function($scope){
+  Auth.initWithLocalStorage = function(){
+      console.log('init with local storage');
       var timeNow = new Date().getTime()/1000;
-      Auth.$scope = $scope;
-
-      if (window.is_signed_in){
-
-
-          var diff = window.authResultexpiration - timeNow;
-          if (diff>0){
-             Auth.processAuth(window.authResult);
+      if (localStorage.getItem("access_token")){
+          var access_token = localStorage.getItem("access_token");
+          var authResultexpiration = localStorage.getItem("authResultexpiration");
+          var diff = authResultexpiration - timeNow;
+          if (diff>0 && access_token!="null"){
+             Auth.$scope.immediateFailed = false;
+             Auth.$scope.isSignedIn = true;
+             gapi.auth.setToken({'access_token':access_token});
+             Auth.$scope.runTheProcess();
           }
           else{
-
+              console.log('w ill refresh');
               // refresh token
               Auth.refreshToken();
-
           }
-
+      // render Google+ sign-in
       }else{
+            console.log('will render sign-in');
+            // check the gapi token
             var isGapiOk = Auth.checkGapiToken();
             if (isGapiOk){
                 var gapiToken = gapi.auth.getToken();
-                Auth.processAuth(window.authResult);
+                Auth.processAuth(gapiToken);
             }else{
               gapi.signin.render('myGsignin', {
                 'callback': Auth.signIn,
@@ -50,22 +53,83 @@ accountservices.factory('Auth', function($http) {
               });
             }
       }
+  }
+  Auth.storeTheToken = function(authResult){
+      if (typeof(Storage) != "undefined") {
+          localStorage['is_signed_in'] = true;
+          localStorage['access_token']=authResult.access_token;
+          localStorage['authResultexpiration'] = authResult.expires_at;
+      }
+      if (!window.access_token) {
+              window.is_signed_in = true;
+              window.access_token = authResult.access_token;
+              window.authResultexpiration =  authResult.expires_at;
+      }
+  }
+  Auth.initSimple = function(){
+      var timeNow = new Date().getTime()/1000;
+      if (window.is_signed_in){
+
+
+          var diff = window.authResultexpiration - timeNow;
+          if (diff>0){
+             Auth.$scope.immediateFailed = false;
+             Auth.$scope.isSignedIn = true;
+             Auth.$scope.runTheProcess();
+          }
+          else{
+
+              // refresh token
+              Auth.refreshToken();
+
+          }
+      // render Google+ sign-in
+      }else{
+            // check the gapi token
+            var isGapiOk = Auth.checkGapiToken();
+            if (isGapiOk){
+                var gapiToken = gapi.auth.getToken();
+                Auth.processAuth(gapiToken);
+            }else{
+              gapi.signin.render('myGsignin', {
+                'callback': Auth.signIn,
+                'clientid': '987765099891.apps.googleusercontent.com',
+                'scope': 'https://www.googleapis.com/auth/userinfo.email',
+                'theme': 'dark',
+                'cookiepolicy': 'single_host_origin',
+                'accesstype': 'online',
+                'width':'wide'
+              });
+            }
+      }
+  }
+  Auth.init = function($scope){
+      // make sure there is only one instance of Auth.init executed
+      if (!window.countInitExec){
+          window.countInitExec = 1;
+      }else{
+          window.countInitExec = window.countInitExec+1;
+          var timeNow = new Date().getTime()/1000;
+          Auth.$scope = $scope;
+          if (typeof(Storage) != "undefined") {
+              // Using the localStorage
+              Auth.initWithLocalStorage();
+          } else {
+              // Using the window object
+              Auth.initSimple();
+          }
+      }
   };
   Auth.signIn = function(authResult){
       Auth.processAuth(authResult);
   };
   Auth.processAuth = function(authResult) {
-      Auth.$scope.immediateFailed = true;
+      //Auth.$scope.immediateFailed = true;
       if (authResult) {
         if (authResult['access_token']){
           Auth.$scope.immediateFailed = false;
           Auth.$scope.isSignedIn = true;
-          if (!window.authResult) {
-              window.is_signed_in = true;
-              window.authResult = authResult;
-              window.authResultexpiration =  authResult.expires_at;
-          }
-
+          Auth.storeTheToken(authResult);
           // run the process
           Auth.$scope.runTheProcess();
         }
@@ -79,6 +143,9 @@ accountservices.factory('Auth', function($http) {
 
   };
   Auth.renderForcedSignIn = function(){
+    if (typeof(Storage) != "undefined") {
+        localStorage.setItem("access_token", null);
+    }
     window.authResult = null;
     Auth.$scope.immediateFailed = true;
     Auth.$scope.$apply();
@@ -89,14 +156,14 @@ accountservices.factory('Auth', function($http) {
       'theme': 'dark',
       'cookiepolicy': 'single_host_origin',
       'accesstype': 'online',
-      'approvalprompt':'force',
       'width':'wide'
     });
   }
   Auth.refreshToken = function(){
 
+    Auth.renderForcedSignIn();
 
-     window.location.reload(true);
+    //window.location.reload(true);
 
   };
 
