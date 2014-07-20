e2c486cf4bba6f6a012303ce31b7925e1da0a427
Belidr09 <bel.idr09@gmail.com>
2014-07-20 15:36:36 +0100

    pull from iogrow
    Former-commit-id: 08727b7d3e7e02ceb9cf018791e6a87200cbd53b [formerly e7132408f90a3202ebe71fbaf49a099cf16fcef6] [formerly d24ad0767dd2c697dfd34424e7a53de32e6a50fb [formerly 62cfdb8d007123cc37f86274c01aee96cea2da79]]
    Former-commit-id: 862519324da70bad7f16eb243fee397888229b9f [formerly 8b1b9589ad627c4a8024c911985fee7ab518d1d0]
    Former-commit-id: 77e8e516172a9c1af71dea50e76f366dd448f13f

---
+++ reporting.py
@@ -0,0 +1,9 @@
+from iomodels.crmengine.leads import Lead
+from model import User
+users=User.query().fetch()
+for user in users:
+	gid=user.google_user_id
+	leads=Lead.query(Lead.owner==gid).fetch()
+	res=[gid,len(leads)]
+
+	
