d858c5ed2a61da432fd6c225430289c14e810335
dixdix <h_hadji@esi.dz>
2014-08-14 10:08:45 +0100

    add stripe handlers , add iogrow-queue to all the taskqueues .
    Former-commit-id: a0ca34ca7861829258082271285f603bf6985c17 [formerly 7272e941e18915648c623f6b3a8dce18e701b5ad] [formerly 21e5e02a45ee54aa1b7a25b8bc687564d098d014 [formerly dc774cbf1500838660aacd6b177ecb917eaf5861]]
    Former-commit-id: 77bc3ae948c2c1dae38999efe8f2bbc12d59e424 [formerly b3a87b3ce739871ee3630255089c05cc2281038a]
    Former-commit-id: 38806c3f181cfec4cdc06803fa18665e998f9467

--- handlers.py
+++ handlers.py
@@ -1209,7 +1209,23 @@ class SendGmailEmail(webapp2.RequestHandler):
                                                 )
         EndpointsHelper.send_message(service,'me',message)
 
-
+# paying with stripe 
+class StripePayingHandler(BaseHandler,SessionEnabledHandler):
+      def post(self):
+          # the secret key .
+          stripe.api_key="sk_test_4Xa3wfSl5sMQYgREe5fkrjVF"
+          # get the token from the client form 
+          token= self.request.get('stripeToken')
+          # charging operation after the payment 
+          try:
+            charge= stripe.Charge.create(
+                amount=1000, 
+                currency="usd",
+                card=token,
+                description="hadji@iogrow.com")
+          except stripe.CardError, e:
+                 # The card has been declined
+                 pass
 
 
 
@@ -1311,7 +1327,9 @@ routes = [
     ('/start-early-bird-account',StartEarlyBird),
     ('/sign-in',SignInHandler),
     ('/sign-up',SignUpHandler),
-    ('/gconnect',GooglePlusConnect)
+    ('/gconnect',GooglePlusConnect),
+    # paying with stripe
+    ('/paying',StripePayingHandler)
     ]
 config = {}
 config['webapp2_extras.sessions'] = {
