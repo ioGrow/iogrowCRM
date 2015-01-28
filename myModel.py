adfe2f231d426794b5a76c1824692f569d367153
Arezki <arezki1990>
2015-01-28 14:41:07 +0300

    fix the delete opportunity in lead
    Former-commit-id: dc387e9f601450b673d7cd4e6609547eef364280 [formerly 449ac85f87805ead309ebee359edb37b5d7eb542] [formerly d09c24055472e3d47f735b9a00d6ead41fec4044 [formerly 6645853d90f894b90c0d569a5238c7cd1bee3b5c]]
    Former-commit-id: 8198d7b22d08d1b755966bf6bc1c997b8a79c441 [formerly d314160cdaaf53bf33aa2a0904dd2526e33ac18c]
    Former-commit-id: 9aa712689228db7f516554fc153cacf9d3d91e11

--- myModel.py
+++ myModel.py
@@ -1,5 +1,8 @@
+from mapreduce import operation as op
 def touch(entity):
     """
     Update the entities timestamp.
     """
-    entity.put()
+    yield op.db.Put(entity)
+    yield op.counters.Increment('touched')
+    
