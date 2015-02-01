f74378c284f9b2c85f4402b78bd3f49f24d58450
dixdix <h_hadji@esi.dz>
2015-02-01 13:44:10 +0300

    delete groupe tab from iogrow company , delete groups tab from tabs table , update add dashboard script so the dashboard cab only added once .
    Former-commit-id: 587f98dfaf3abeed305989db86e7c8c98db6559f [formerly 1b44a2b0bc8f1b4535a8727ce94dcf956b514362] [formerly 77a056269b4eca910da59f7b62626fc6add525f8 [formerly d987fb0b029ef60ff8a686a0a22e2fa42c420cd5]]
    Former-commit-id: a103a72f438c8a824f3d2acd27944583fa67b9a5 [formerly cdf3ba74d517b4e07804dfb2382ddc72308413e6]
    Former-commit-id: 0cf6af3255fa7d2f406a5832c6de38b987a88d67

--- myModel.py
+++ myModel.py
@@ -1,4 +1,4 @@
-from mapreduce import operation as op
+from mapreduce import operation as op,context
 from model import Tab
 
 def is_admin(entity):
@@ -28,12 +28,31 @@ def touch(entity):
     yield op.counters.Increment('touched')
 
 def add_dashboard_tab(entity):
+    add_dashboard_tab=True
     if entity.name=="sales":
-       org_key=entity.organization
-       created_tab=Tab(name='Dashboard',label='Dashboard',url='/#/dashboard/',icon='dashboard',organization=org_key)
-       tab_key=created_tab.put()
-       entity.tabs.append(tab_key)
+      for x in xrange(1,len(entity.tabs)):
+           tab=entity.tabs[x].get()
+           if tab.name=="Dashboard":
+              add_dashboard_tab=False
+      if add_dashboard_tab:
+        org_key=entity.organization
+        created_tab=Tab(name='Dashboard',label='Dashboard',url='/#/dashboard/',icon='dashboard',organization=org_key)
+        tab_key=created_tab.put()
+        entity.tabs.append(tab_key)
     yield op.db.Put(entity)
     yield op.counters.Increment('touched')
 
+def delete_tab_group_from_datastore(entity):
+      if entity.name=="Groups":
+        yield op.db.Delete(entity)
+        yield op.counters.Increment('touched')
 
+def delete_iogrow_groups_tab(entity):
+    params = context.get().mapreduce_spec.mapper.params
+    iogrow_organization_key=params.get('organization_key')
+    if entity.name=="admin":
+       if iogrow_organization_key== entity.organization.urlsafe():
+          entity.tabs.pop(1) 
+    yield op.db.Put(entity)
+    yield op.counters.Increment('touched')
+   
