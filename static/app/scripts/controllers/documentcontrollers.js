57c53a36d0e18335ad4961419fab781d5646482a
hakim <h.karriche@werework.com>
2015-02-08 14:54:42 +0300

    new isloadin process on Document show
    Former-commit-id: e04da4fba7e296616ed89b254ddf8f0ec36604d3 [formerly b14d70956e1d7bc6c29dcf6cee4fde7dec4f3f6c] [formerly 1dbb4be998f33318a8d377d335f37398892b8521 [formerly 609fabaa46f05ee3d00ee7a695d9d5b273844c51]]
    Former-commit-id: d431ddd16ae374ddbed66e637095c5713c990506 [formerly 0ee4f70c037747872930d25ffb3f6415da40b99c]
    Former-commit-id: 4754948c85ab492d260da1bcdba7f10c783d7bcb

--- static/app/scripts/controllers/documentcontrollers.js
+++ static/app/scripts/controllers/documentcontrollers.js
@@ -17,6 +17,34 @@ app.controller('DocumentShowController',['$scope','$filter','$route','Auth','Att
  $scope.editbeforedelete = function(){
     $('#BeforedeleteAttachement').modal('show');
    };
+    $scope.inProcess=function(varBool,message){
+          if (varBool) {   
+            if (message) {
+              console.log("starts of :"+message);
+             
+            };
+            $scope.nbLoads=$scope.nbLoads+1;
+            if ($scope.nbLoads==1) {
+              $scope.isLoading=true;
+            };
+          }else{
+            if (message) {
+              console.log("ends of :"+message);
+            };
+            $scope.nbLoads=$scope.nbLoads-1;
+            if ($scope.nbLoads==0) {
+               $scope.isLoading=false;
+            };
+          };
+        }       
+       
+        $scope.apply=function(){
+         
+          if ($scope.$root.$$phase != '$apply' && $scope.$root.$$phase != '$digest') {
+               $scope.$apply();
+              }
+              return false;
+        }
 
       // What to do after authentication
      $scope.runTheProcess = function(){
