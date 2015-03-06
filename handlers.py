837acaf1fd290743e8f66885d3a2c9cef9ef5e1a
Tedj Meabiou <tedj.meabiou@gmail.com>
2015-03-07 01:12:12 +0400

    adding growth hacking as discovery topic by default
    Former-commit-id: 87500669431f4abd3bfbb126253cda5163c66e89 [formerly 6a337386d182da34fc6ac14ddc78cca466505e3a] [formerly d58a24529952518007ae504b7b94784eb52c6f7d [formerly ea25aa078d8601a382b02a64505f2c449e549954]]
    Former-commit-id: 1ee79741e45e3c3fb542eb81bd220f5c6ec2ceb8 [formerly bd811d9aa6111bd75baf07ab0a4c9e929f6fe38f]
    Former-commit-id: beafd624ac3e4fef743801c6a4a61dfd4e3f90bb

--- handlers.py
+++ handlers.py
@@ -527,7 +527,7 @@ class SignUpHandler(BaseHandler, SessionEnabledHandler):
             tagschema=Tag()
             tagschema.organization = org_key
             tagschema.owner = user.google_user_id
-            tagschema.name=org_name
+            tagschema.name="growth hacking"
             tagschema.about_kind="topics"
             tagschema.color=random.choice(colors)
             tagschema.put()
