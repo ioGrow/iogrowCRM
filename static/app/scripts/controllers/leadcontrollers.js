07eae9514ce72853471dcaaf0399888aeaa64d8e
Yacine Hamidia <y_hamidia@esi.dz>
2015-03-11 11:23:05 +0300

    fixing bug on delete items without reload the page
    Former-commit-id: 1fff9722fc2de871b5b13b24aa0c0cc66d165f19 [formerly 993080a672123cd95a4c5c3030ddcdb29e4e2da1] [formerly 592a2ca457064359b03db8d9e8005a95d74c676e [formerly 1aa2c27280f28b7117c2f67e8b50593a5d9c6dd7]]
    Former-commit-id: 64269463b1f5bdab39311e5988e8f5963ad23833 [formerly fd8f3408e20ef065148a37dfb0dc69ee5da03eab]
    Former-commit-id: 9e0503e023d34cd2804836ca42cdc153b41e64d6

--- static/app/scripts/controllers/leadcontrollers.js
+++ static/app/scripts/controllers/leadcontrollers.js
@@ -116,6 +116,19 @@ app.controller('LeadListCtrl', ['$scope','$filter','Auth','Lead','Leadstatus','T
           window.Intercom('update');
 
         };
+          $scope.leadDeleted=function(){
+            if (!jQuery.isEmptyObject($scope.selectedLead)&&$scope.selectedContact!=null) {  
+               $scope.leads.splice($scope.leads.indexOf($scope.selectedLead) , 1);
+               $scope.apply();
+            }else{
+              angular.forEach($scope.selectedCards, function(selected_lead){
+                  $scope.leads.splice($scope.leads.indexOf(selected_lead) , 1);
+                  $scope.apply();
+              });
+               $scope.selectedCards=[];
+            };
+            
+          }
               $scope.gotosendMail = function(email,lead){
                    $scope.leadToMail=lead;
                    $scope.email.to = email;
@@ -150,7 +163,37 @@ app.controller('LeadListCtrl', ['$scope','$filter','Auth','Lead','Leadstatus','T
                    $scope.smallModal=false;
               };
             }
-            
+                  $scope.showAttachFilesPicker = function() {
+          var developerKey = 'AIzaSyDHuaxvm9WSs0nu-FrZhZcmaKzhvLiSczY';
+          var docsView = new google.picker.DocsView()
+              .setIncludeFolders(true)
+              .setSelectFolderEnabled(true);
+          var picker = new google.picker.PickerBuilder().
+              addView(new google.picker.DocsUploadView()).
+              addView(docsView).
+              setCallback($scope.attachmentUploaderCallback).
+              setOAuthToken(window.authResult.access_token).
+              setDeveloperKey(developerKey).
+              setAppId('935370948155-qm0tjs62kagtik11jt10n9j7vbguok9d').
+                enableFeature(google.picker.Feature.MULTISELECT_ENABLED).
+              build();
+          picker.setVisible(true);
+      };
+      $scope.attachmentUploaderCallback= function(data){
+        if (data.action == google.picker.Action.PICKED) {
+                
+
+                $.each(data.docs, function(index) {
+                    var file = { 'id':data.docs[index].id,
+                                  'title':data.docs[index].name,
+                                  'mimeType': data.docs[index].mimeType,
+                                  'embedLink': data.docs[index].url
+                    };
+                    $scope.sendWithAttachments.push(file);
+                });
+                $scope.apply();
+        }
+      }
             $scope.smallSendMail=function(){
               $(".modal-backdrop").remove();
               $('#testnonefade').addClass("emailModalOnBottom");
@@ -267,17 +310,7 @@ app.controller('LeadListCtrl', ['$scope','$filter','Auth','Lead','Leadstatus','T
               });             
               $('#BeforedeleteSelectedLeads').modal('hide');
           };
-          $scope.leadDeleted = function(resp){
-
-            if ($scope.selectedCards.length >0) {
-              angular.forEach($scope.selectedCards, function(selected_lead){
-                 $scope.leads.splice($scope.leads.indexOf(selected_lead) , 1);
-                }); 
-            };        
-              $scope.selectedCards=[];
-          };
           $scope.selectCardwithCheck=function($event,index,lead){
-              console.log("wwwwwwwwwwwwwwwwwoer");
               var checkbox = $event.target;
 
                if(checkbox.checked){
@@ -1084,7 +1117,7 @@ $scope.checkScrollBar=function(){
        $scope.isbigScreen=true;
     }
 
-   $scope.$apply();    
+   $scope.apply();    
 
 }
 
@@ -1274,7 +1307,9 @@ app.controller('LeadShowCtrl', ['$scope','$filter','$route','Auth','Email', 'Tas
                     };
                     return match;
         }
-
+      $scope.leadDeleted=function(){
+          window.location.replace('#/leads');
+      }
       $scope.runTheProcess = function(){
             var params = {
                           'id':$route.current.params.leadId,
