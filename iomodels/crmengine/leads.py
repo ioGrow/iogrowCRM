6219c11ef9c51fd97b1a0048463c1d050b3bb1c1
arezki1990 <arezki@iogrow.com>
2015-05-03 14:36:34 +0300

    genirelize owner and access fields on all tabs
    Former-commit-id: d73d43c67b12eaf24fde0975781a11da46db4659 [formerly 3910462dd0bacf2925bc038e04992ce7d0abceef] [formerly 094c02e8e396146a234ce49c51cda62b36f64145 [formerly 5322dba97360400062a0f2dddda6c70f79bb4d08]]
    Former-commit-id: 3d8fc2fadf0878ad9453c664cd621ef3170acdd2 [formerly ce776980de08fe49d9f1f5731584eabdcb948302]
    Former-commit-id: 59f53778006c2184c0775a41c7057659ffaaf7f8

--- iomodels/crmengine/leads.py
+++ iomodels/crmengine/leads.py
@@ -494,6 +494,15 @@ class Lead(EndpointsModel):
                     is_filtered = False
                 if is_filtered and Node.check_permission( user_from_email, lead ):
                     tag_list = Tag.list_by_parent(parent_key = lead.key)
+                    owner = model.User.get_by_gid(lead.owner)
+                    owner_schema = iomessages.UserSchema(
+                                        id = str(owner.id),
+                                        email = owner.email,
+                                        google_display_name = owner.google_display_name,
+                                        google_public_profile_photo_url=owner.google_public_profile_photo_url,
+                                        google_public_profile_url=owner.google_public_profile_url,
+                                        google_user_id = owner.google_user_id
+                                        )
                     lead_schema = LeadSchema(
                                       id = str( lead.key.id() ),
                                       entityKey = lead.key.urlsafe(),
@@ -502,6 +511,8 @@ class Lead(EndpointsModel):
                                       title = lead.title,
                                       company = lead.company,
                                       tags = tag_list,
+                                      owner=owner_schema,
+                                      access=lead.access,
                                       profile_img_id = lead.profile_img_id,
                                       profile_img_url = lead.profile_img_url,
                                       linkedin_url = lead.linkedin_url,
