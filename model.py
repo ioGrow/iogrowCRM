6eb3c120bcdb5c39ab880b2218c3a5a7ac71ba58
Tedj Meabiou <tedj.meabiou@gmail.com>
2015-06-01 12:35:33 +0400

    adding CustomField Model
    Former-commit-id: bc8a809f213fb66d1ed8917d5c11813e32973826 [formerly c4fb15d9a9e6cab9006074ae47c389b047d8e11c] [formerly dba4929a3feb44eb7d9b4da306e74a0ed91f8c02 [formerly 066cfd1c996384e9a2fc1fcbd44a5b5390dbbad6]]
    Former-commit-id: 89906e819198a57fa50370843ec1264534251714 [formerly 3814bffbe044f744065ed18d025f98abcf1a9e32]
    Former-commit-id: 0f7fb98de56b94784ba104e5fdea331ec69b1e6c

--- model.py
+++ model.py
@@ -183,6 +183,21 @@ class LicenseModel(ndb.Model):
     is_free =  ndb.BooleanProperty()
     duration = ndb.IntegerProperty()
 
+class CustomField(ndb.Model):
+    name = ndb.StringProperty()
+    related_object = ndb.StringProperty()
+    field_type = ndb.StringProperty()
+    help_text = ndb.StringProperty()
+    options = ndb.StringProperty(repeated=True)
+    scale_min = ndb.IntegerProperty()
+    scale_max = ndb.IntegerProperty()
+    label_min = ndb.StringProperty()
+    label_max = ndb.StringProperty()
+    owner = ndb.StringProperty()
+    organization = ndb.KeyProperty()
+    created_at = ndb.DateTimeProperty(auto_now_add=True)
+    updated_at = ndb.DateTimeProperty(auto_now=True)
+
 # We use the Organization model to separate the data of each organization from each other
 class Organization(ndb.Model):
     owner = ndb.StringProperty()
