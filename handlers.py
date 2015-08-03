907f0cf7d89012189e240f0170004934c03bc708
Tedj Meabiou <tedj.meabiou@gmail.com>
2015-08-03 18:21:05 +0100

    fixing some bugs on salesforce extension mark as lead apis
    Former-commit-id: 66c60193e15a39277ab8e906b39a95497bd9ee34 [formerly c205d4575ad5a797ead1ab95841c0030017cbb0c] [formerly 8c50185866737c2fe5a819c84b41a038b3b61d9b [formerly a207573cb7f8f705e0e291185c21eec4e72920b6]]
    Former-commit-id: 487f066d91b8ebeb7b9b32e4297b2903f6903325 [formerly 96aa37e071d90d0a752e7db84248e5431fbd4075]
    Former-commit-id: 7d49e40534b07ce867e7de24d6e5a01051032fff

--- handlers.py
+++ handlers.py
@@ -1218,6 +1218,49 @@ class SFmarkAsLead(BaseHandler, SessionEnabledHandler):
                     'Title':smart_str(title)
                     }
             if street!='':
+                # countries = ['United States', 'Afghanistan', 'Aland Islands', 'Albania',
+                #  'Algeria', 'American Samoa', 'Andorra', 'Angola', 'Anguilla', 'Antarctica', 
+                #  'Antigua and Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria',
+                #   'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 
+                #   'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia and Herzegovina', 'Botswana', 
+                #   'Bouvet Island', 'Brazil', 'British Indian Ocean Territory', 'Brunei Darussalam', 'Bulgaria',
+                #    'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Caribbean Nations', 
+                #    'Cayman Islands', 'Central African Republic', 'Chad', 'Chile', 'China', 'Christmas Island', 
+                #    'Cocos (Keeling) Islands', 'Colombia', 'Comoros', 'Congo', 'Cook Islands', 'Costa Rica', 
+                #    "Cote D'Ivoire (Ivory Coast)", 'Croatia', 'Cuba', 'Cyprus', 'Czech Republic',
+                #     'Democratic Republic of the Congo', 'Denmark', 'Djibouti', 'Dominica', 
+                #     'Dominican Republic', 'East Timor', 'Ecuador', 'Egypt', 'El Salvador', 
+                #     'Equatorial Guinea', 'Eritrea', 'Estonia', 'Ethiopia', 'Falkland Islands (Malvinas)', 
+                #     'Faroe Islands', 'Federated States of Micronesia', 'Fiji', 'Finland', 'France', 'French Guiana', 
+                #     'French Polynesia', 'French Southern Territories', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 
+                #     'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guadeloupe', 'Guam', 'Guatemala', 'Guernsey', 'Guinea',
+                #      'Guinea-Bissau', 'Guyana', 'Haiti', 'Heard Island and McDonald Islands', 'Honduras', 'Hong Kong', 
+                #      'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 
+                #      'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', 'Korea', 
+                #      'Korea (North)', 'Kosovo', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 
+                #      'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macao', 'Macedonia', 
+                #      'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall Islands', 'Martinique', 
+                #      'Mauritania', 'Mauritius', 'Mayotte', 'Mexico', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 
+                #      'Montserrat', 'Morocco', 'Mozambique', 'Myanmar', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 
+                #      'Netherlands Antilles', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Niue',
+                #       'Norfolk Island', 'Northern Mariana Islands', 'Norway', 'Pakistan', 'Palau', 'Palestinian Territory',
+                #        'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Pitcairn', 'Poland', 'Portugal',
+                #         'Puerto Rico', 'Qatar', 'Reunion', 'Romania', 'Russian Federation', 'Rwanda', 
+                #         'S. Georgia and S. Sandwich Islands', 'Saint Helena', 'Saint Kitts and Nevis',
+                #          'Saint Lucia', 'Saint Pierre and Miquelon', 'Saint Vincent and the Grenadines', 
+                #          'Samoa', 'San Marino', 'Sao Tome and Principe', 'Saudi Arabia', 'Senegal', 'Serbia', 
+                #          'Serbia and Montenegro', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovak Republic', 
+                #          'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 'South Sudan', 'Spain', 
+                #          'Sri Lanka', 'Sudan', 'Sultanate of Oman', 'Suriname', 'Svalbard and Jan Mayen', 'Swaziland',
+                #           'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', 'Timor-Leste', 
+                #           'Togo', 'Tokelau', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 
+                #           'Turks and Caicos Islands', 'Tuvalu', 'Uganda', 'Ukraine', 'United Arab Emirates', 
+                #           'United Kingdom', 'Uruguay', 'Uzbekistan', 'Vanuatu', 'Vatican City State (Holy See)', 
+                #           'Venezuela', 'Vietnam', 'Virgin Islands (British)', 'Virgin Islands (U.S.)', 
+                #           'Wallis and Futuna', 'Western Sahara', 'Yemen', 'Yugoslavia', 'Zambia', 'Zimbabwe', 'Other']
+                # address_fields = street.split(',')
+                # if address_fields[-1].strip() in countries:
+                #     params['Country']=address_fields[-1].strip()
                 params['Street']=street.encode('ascii', 'ignore').decode('ascii')
             if introduction!='':
                 params['Description']=smart_str(introduction)
