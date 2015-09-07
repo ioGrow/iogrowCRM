543be80a66bac1092be455f5aff4495fef8296d7
Tedj Meabiou <tedj.meabiou@gmail.com>
2015-09-07 14:23:08 +0100

    fix #2028 opportunity.insert add multiple contacts
    Former-commit-id: 708a2ab016c8eb3386bea01a679c5d1b72540e3d [formerly 3fe757c977a07c1e20ad1daf71ec6c104b53e2d8] [formerly 2a82d9e8d8ec1f7abe5b6a0e19e619c88ce6c944 [formerly d6aa9d7314cb3ed0f3e6396f4a89a7a56a541ba2]]
    Former-commit-id: e3bce86c76f753cbfd05f17f8f96513d83126903 [formerly c341ad505e5fa5160d4fd010c5755a9d65220f3b]
    Former-commit-id: 4b0cbacadae99e750abc0c1f3edce65da7d756f7

--- iomodels/crmengine/opportunities.py
+++ iomodels/crmengine/opportunities.py
@@ -62,6 +62,7 @@ class OpportunityInsertRequest(messages.Message):
     decission_process = messages.StringField(21)
     time_scale = messages.StringField(22)
     need = messages.StringField(23)
+    contacts = messages.StringField(24,repeated=True)
 
 class OpportunityPatchRequest(messages.Message):
     id = messages.StringField(1)
@@ -1163,6 +1164,42 @@ class Opportunity(EndpointsModel):
                     data = EndpointsHelper.get_data_from_index(str( account.key.id() ))
                     account.put_index(data)
         contact = None
+        for c in request.contacts:
+            try:
+                contact_key = ndb.Key(urlsafe=c)
+                contact = contact_key.get()
+            except:
+                from iomodels.crmengine.contacts import Contact
+                contact_key = Contact.get_key_by_name(
+                                                    user_from_email= user_from_email,
+                                                    name = c
+                                                    )
+                if contact_key:
+                    contact=contact_key.get()
+                else:
+                    firstname = request.contact.split()[0]
+                    lastname = " ".join(request.contact.split()[1:])
+                    contact = Contact(
+                                    firstname=firstname,
+                                    lastname=lastname,
+                                    owner = user_from_email.google_user_id,
+                                    organization = user_from_email.organization,
+                                    access = request.access
+                                    )
+                    contact_key_async = contact.put_async()
+                    contact_key = contact_key_async.get_result()
+                    if account:
+                        data = EndpointsHelper.get_data_from_index(str( contact.key.id() ))
+                        contact.put_index(data)
+                        Edge.insert(start_node = account.key,
+                          end_node = contact.key,
+                          kind = 'contacts',
+                          inverse_edge = 'parents')
+                        EndpointsHelper.update_edge_indexes(
+                                                        parent_key = contact.key,
+                                                        kind = 'contacts',
+                                                        indexed_edge = str(account.key.id())
+                                                        )
         if request.contact:
             try:
                 contact_key = ndb.Key(urlsafe=request.contact)
