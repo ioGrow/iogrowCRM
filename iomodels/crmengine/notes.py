c2e3aad11c5489e180d50c9e4aeb6cf41b450486
bigali <bigalitux@gmail.com>
2015-09-10 17:27:50 +0100

    add index to note object
    Former-commit-id: 4b58e3b5df7fa046130ff219bd3d7c2e5c05846d [formerly 42c08e779b9ca2600b4e6ad8b2f5cace04fdaaaf] [formerly 9b2023daf29c8290c673cc798a5b48a427aac601 [formerly f903aff4a44eef01eaccbf9f6d9b304a26ca7d19]]
    Former-commit-id: 2ddd2a85a3f24f9c28f6b1d38f4269d11c23c3d4 [formerly 2a4408bf1746c8ee46f2c48978be91c4e7711689]
    Former-commit-id: f86dc0c8a89e7f2cfd6d7f02b9da3a0e47d47674

--- iomodels/crmengine/notes.py
+++ iomodels/crmengine/notes.py
@@ -202,6 +202,53 @@ class Note(EndpointsModel):
 
 
     @classmethod
+    def insert(cls, user_from_email, request):
+        parent_key = ndb.Key(urlsafe=request.about)
+        note_author = Userinfo()
+
+        note_author.display_name = user_from_email.google_display_name
+        note_author.photo = user_from_email.google_public_profile_photo_url
+        note = Note(
+            owner=user_from_email.google_user_id,
+            organization=user_from_email.organization,
+            author=note_author,
+            title=request.title,
+            content=request.content
+        )
+        entityKey_async = note.put_async()
+        entityKey = entityKey_async.get_result()
+
+        # entityKey= note.put()
+
+        note.put_index()
+
+        Edge.insert(
+            start_node=parent_key,
+            end_node=entityKey,
+            kind='topics',
+            inverse_edge='parents'
+        )
+
+        author_shema = AuthorSchema(
+            google_user_id=note.owner,
+            display_name=note_author.display_name,
+            google_public_profile_url=note_author.google_public_profile_url,
+            photo=note_author.display_name,
+            edgeKey="",
+            email=note_author.email
+        )
+
+        note_schema = NoteSchema(
+            id=str(note.key.id()),
+            entityKey=note.key.urlsafe(),
+            title=note.title,
+            content=note.content,
+            created_by=author_shema
+        )
+        return note_schema
+
+
+    @classmethod
     def list_by_parent(cls,parent_key,request):
      
         topic_list = []
