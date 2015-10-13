050a123b3e4c2c958c1fe4982fc4075b026d4647
y-hamidia <y_hamidia@esi.dz>
2015-10-13 21:30:48 +0100

    working on custom fields
    Former-commit-id: 61f6f943d18ab3a1d31274768ad20cdfe07b96d6 [formerly 5ccf452461b6aef3a170df43ab43a67cab7b48f5] [formerly d939ad8df3c0a2d033068b12ae5a3560d09ddaa9 [formerly 9225249a8a8fc19dcdee3753f49f4a578f12bd01]]
    Former-commit-id: aab4da506dff967f05327de9087e9a0e56d3de04 [formerly 24ef38bc54dc0256fd21e49c0302d75384d8009e]
    Former-commit-id: 7a997fe1634c6866e6786563e66c21812a1368af

--- static/app/scripts/controllers/admin/CustomFieldsEditController.js
+++ static/app/scripts/controllers/admin/CustomFieldsEditController.js
@@ -1,9 +1,78 @@
-/**
- * Created by Ghiboub khalid on 9/29/15.
- */
-
 app.controller('CustomFieldsEditCtrl', ['$scope', 'Auth', 'User', 'Map',
     function ($scope, Auth, User, Map) {
+    	$scope.customfield={};
+    	$scope.customfield.options=[];
+    	$scope.customfields=[];
         $("ul.page-sidebar-menu li").removeClass("active");
         $("#id_CustomFields").addClass("active");
+        $scope.addOption=function(options){
+        	console.log("clicked")
+        	var option="Option "+(options.length+1);
+        	if (option!=""&&options.indexOf(option)<0) {
+        		options.push(option);
+        	};
+        	$scope.customfield.newOption="";
+        }
+        $scope.countArray=function(options){
+        	return options.length+1;
+        }
+        $scope.removeOption=function(options,option){
+        	if (option!="") {
+        		options.splice(options.indexOf(option));
+        	};	
+        }
+        $scope.udpateOption=function(index){
+        	$scope.customfield.options[index]=$('#option-'+index).val();
+        }
+        $scope.buildElement=function(customfield){
+        	console.log('fired');
+        	// $('#elementContainer').empty();
+        	var htmlElement=null;
+         	var type=null;
+        	switch (customfield.type) {
+                    case 'text' :
+                    	htmlElement='<input/>';
+                    	type: 'text'
+                        break;
+                    case 'paragraph' :
+                    	htmlElement='<textarea/>'
+                        break;
+                    case 'multiple_choice' :
+                    	htmlElement='<input/>';
+                    	type='radio'
+                        break;
+                    case 'checkbox' :
+                    	htmlElement='<input>';
+                    	type: 'text'
+                        break;
+                    case 'list' :
+                    	htmlElement='<select/>'
+                        break;
+	                    }
+	        var props={
+	        	id:'previewElement',
+	        	class:'form-control'
+	        }
+	        if (type!=null) {
+	        	props.type=type;
+	        };
+        	// jQuery(htmlElement,props).appendTo('#elementContainer');
+			if (htmlElement=="<select/>"&&customfield.options.length>0) {
+				// for (var i = 0; i < customfield.options.length; i++) {
+				// 	jQuery('<option/>', {
+				// 			    id: 'foo',
+				// 			    value:customfield.options[i],
+				// 			    text:customfield.options[i]
+				// 			}).appendTo('#previewElement');
+				// };
+				// jQuery('<option/>', {
+				// 			    ngRepeat:"option in customfield.options",
+				// 			    text:'<%=option%>'
+				// 			}).appendTo('#previewElement');
+			};
+        }
+        $scope.$watch('customfield.options', function(newValue, oldValue) {
+		      console.log("value");
+			 });
+
     }]);
