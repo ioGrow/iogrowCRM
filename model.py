3c8fd483f6e853756960c5622c91cb9f51b1b367
hkarriche <h.karriche@werework.com>
2016-03-08 09:02:57 +0100

    model
    Former-commit-id: 23aeff7604e3517d298bfffa5dc37265a88ea352 [formerly eddc0ca83baaba355f9b91c27969cb3c710c1ed7] [formerly 26dc1c916dc63cd68af2be3fcfe6db1f1cda36dd [formerly 49a4568bbd9338b4f6c46cdff86cd9899b8a53ca]]
    Former-commit-id: 04b78c902db49810839df5e75c1b63b1dab1b820 [formerly 3a894dfc5f78985453e8f6932917f6b5aeaf78a3]
    Former-commit-id: 3c85f0de41ec090a71114f2cd1aa3a17ec01e831

--- model.py
+++ model.py
@@ -361,12 +361,7 @@ class Organization(ndb.Model):
 
     def get_subscription(self):
         if not self.subscription:
-            if self.plan:
-                licence = self.plan.get()
-                if licence.name == "life_time_free":
-                    self.subscription = Subscription.create_life_free_subscription().key
-            else:
-                self.subscription = Subscription.create_freemium_subscription().key
+            self.subscription = Subscription.create_freemium_subscription().key
             self.put()
         return self.subscription.get()
 
@@ -437,10 +432,6 @@ class Organization(ndb.Model):
         else:
             cls.init_freemium_licenses(org_key)
 
-    # @classmethod
-    # def init_freemium_licenses(cls, org_key):
-    #     cls.init_life_time_free_licenses(org_key)
-
     @classmethod
     def init_default_values(cls, org_key):
         # HKA 17.12.2013 Add an opportunity stage
@@ -471,6 +462,7 @@ class Organization(ndb.Model):
             owner=admin.google_user_id,
             name=org_name,
             nb_used_licenses=1,
+            subscription=Subscription.create_freemium_subscription().key
         )
         org_key = organization.put()
         mp.track(admin.id, 'SIGNED_UP_SUCCESS')
@@ -547,6 +539,7 @@ class Organization(ndb.Model):
                 created_profile.put()
         # create reports details
 
+
         # init default stages,status, default values...
         cls.init_default_values(org_key)
         if license_type == 'premium_trial':
@@ -566,7 +559,13 @@ class Organization(ndb.Model):
         admin.license_expires_on = now_plus_month
         admin.is_admin = True
         admin.put()
-        organization.subscription = organization.get_subscription().key
+        # taskqueue.add(
+        #             url='/workers/initreport',
+        #             queue_name='iogrow-low',
+        #             params={
+        #                     'admin': admin.key.urlsafe()
+        #                     })
+
         return org_key
 
     @classmethod
