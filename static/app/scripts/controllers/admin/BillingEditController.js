407e5742454d734abee818acc1a42438899b0030
Khalid GHIBOUB <kghiboub@gmail.com>
2016-04-06 10:28:01 +0100

    clean  billingEditController and billing_edit.html
    Former-commit-id: d8948b503cfd431f0b42b4eb470fa1c5cc90873c [formerly 7d1d259535dd80f8a09729a8694f06daec8a7ab7] [formerly 25c3f13b3f0eb849e2b2fe6358fbb196ecdf4a36 [formerly 0b2ed98056e5b1f5f210e153b9e4e2fe43a9e787]]
    Former-commit-id: 81b4100c89f481b48866f360d95765b3db38b9c7 [formerly 5213352db677ba0e52693f864b034072557575ac]
    Former-commit-id: 6027ed4b591291048428aae1866565c233814230

--- static/app/scripts/controllers/admin/BillingEditController.js
+++ static/app/scripts/controllers/admin/BillingEditController.js
@@ -1,34 +1,21 @@
 app.controller('BillingEditCtrl', ['$scope', 'Auth', 'Billing', function ($scope, Auth, Billing) {
     $("ul.page-sidebar-menu li").removeClass("active");
     $("#id_Billing").addClass("active");
-    $scope.isSignedIn = false;
     $scope.immediateFailed = false;
     $scope.nbLoads = 0;
     $scope.isLoading = false;
-    $scope.nbrSelected = 0;
     $scope.inProcess = function (varBool, message) {
         if (varBool) {
-            if (message) {
-                console.log("starts of :" + message);
-            }
-            ;
             $scope.nbLoads = $scope.nbLoads + 1;
             if ($scope.nbLoads == 1) {
                 $scope.isLoading = true;
             }
-            ;
         } else {
-            if (message) {
-                console.log("ends of :" + message);
-            }
-            ;
             $scope.nbLoads = $scope.nbLoads - 1;
             if ($scope.nbLoads == 0) {
                 $scope.isLoading = false;
             }
-            ;
         }
-        ;
     };
     $scope.apply = function () {
         if ($scope.$root.$$phase != '$apply' && $scope.$root.$$phase != '$digest') {
@@ -53,11 +40,9 @@ app.controller('BillingEditCtrl', ['$scope', 'Auth', 'Billing', function ($scope
     $scope.editCardInfo = function () {
         $('#edit_card_info').modal('show');
     };
-    // What to do after authentication
     $scope.runTheProcess = function () {
         Billing.getSubscription($scope);
     };
-    // We need to call this to refresh token when user credentials are invalid
     $scope.refreshToken = function () {
         Auth.refreshToken();
     };
