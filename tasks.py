e97c36496b5bfe64d2a292fee636e93496f8b68c
Assem Chelli <assem.ch@gmail.com>
2016-04-12 10:53:47 +0100

    rename run to start and invoke.run_ back to run
    Former-commit-id: 548f93833a3c29fa09182404920b4dacf62f198b [formerly 26df19b0c70e26dbd6dec7187078853e2092def9] [formerly d411a0f18a0d59272165d46a5422ab3d511414d0 [formerly b42f9a6f39206448342d7d7c96726147a240b1ef]]
    Former-commit-id: 58c499868dcdf0eb7ac1289358ff9e824277bc64 [formerly 648f980bbf21474cdd248e78f55dbaa54b7c1867]
    Former-commit-id: 6fc2393520a480f38584a8b0dfe5fecc57b129d6

--- tasks.py
+++ tasks.py
@@ -1,61 +1,61 @@
 from invoke import task
-from invoke import run as run_
+from invoke import run
 
 
 @task
 def clean():
 	if raw_input("Are you sure to run clean, you will lose any file not added to git? y/[n]") == "y":
-		run_("git clean -xfd")
+		run("git clean -xfd")
 
 @task 
 def pylint():
-	run_("pylint --ignore=lib ./* -f colorized | more")
+	run("pylint --ignore=lib ./* -f colorized | more")
 
 @task 
 def babel_extract():
-	run_("pybabel extract -F ./babel.cfg -o ./locale/messages.pot ./")
+	run("pybabel extract -F ./babel.cfg -o ./locale/messages.pot ./")
 
 @task 
 def babel_init():
 	lang = raw_input()
-	run_("pybabel init -l %s -d ./locale -i ./locale/messages.pot" % lang)
+	run("pybabel init -l %s -d ./locale -i ./locale/messages.pot" % lang)
 
 @task 
 def babel_compile():
-	run_("pybabel compile -f -d ./locale")
+	run("pybabel compile -f -d ./locale")
 
 @task     
 def babel_update():
 	lang = raw_input()
-	run_("pybabel update -l %s -d ./locale -i ./locale/messages.pot" % lang)
+	run("pybabel update -l %s -d ./locale -i ./locale/messages.pot" % lang)
 
 
 @task
 def install_dep():
-	run_("pip install -r requirements.txt --upgrade --target ./libx")
+	run("pip install -r requirements.txt --upgrade --target ./libx")
 
 @task 
 def install_grunt():
-	run_("sudo npm i -g grunt")
-	run_("npm i grunt-contrib-watch")
-	run_("npm i grunt-contrib-concat")
-	run_("npm i grunt-contrib-uglify")
-	run_("npm i grunt-contrib-cssmin")
-	run_("npm i grunt-contrib-htmlmin")
+	run("sudo npm i -g grunt")
+	run("npm i grunt-contrib-watch")
+	run("npm i grunt-contrib-concat")
+	run("npm i grunt-contrib-uglify")
+	run("npm i grunt-contrib-cssmin")
+	run("npm i grunt-contrib-htmlmin")
 
 @task
 def grunt():
-	run_("grunt concat; grunt uglify; grunt cssmin") #grunt
+	run("grunt concat; grunt uglify; grunt cssmin") #grunt
 
 @task(grunt)
 def build():
 	pass
 
 @task(build)
-def run(gae_path="../google_appengine/"):
-    run_(gae_path+"dev_appserver.py ./ --port 8090")
+def start(gae_path="../google_appengine/"):
+    run(gae_path + "dev_appserver.py ./ --port 8090")
 
 @task
 def deploy(gae_path="../google_appengine/"):
-	run_(gae_path+"appcfg.py update ./")
+	run(gae_path + "appcfg.py update ./")
 
