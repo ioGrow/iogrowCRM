80a3449d7a12f23af529992b9395c513b5d972fa
Assem Chelli <assem.ch@gmail.com>
2016-04-19 17:14:48 +0100

    Update tasks
    Former-commit-id: e0c353a5db4cc14b0ea7f5d7b6e9e1a0f992b272 [formerly 420f9c1915c68c328c41efe958294e5f490bc273] [formerly 9c6766a8d8adbf51e20bc39a7d54843bca207bbc [formerly 9c90e910e0f84e77b6d0279e8a7522464682e5ea]]
    Former-commit-id: 6bb1432288d490e55806da56d70cd19d97c5e88b [formerly 2b12171f09a596242cfbecf2e9dbac3e5fb92c23]
    Former-commit-id: 67291760b516ff686e05cfa39ba472b19dabf6bf

--- tasks.py
+++ tasks.py
@@ -2,71 +2,47 @@ from invoke import task
 from invoke import run
 
 
+@task(default=True)
+def default():
+    run("invoke --list")
+
+@task
+def install():
+    run("sudo npm i -g grunt")
+    run("sudo npm i -g bower")
+    run("npm i")
+    run("pip install -r requirements.txt --upgrade --target ./libx")  #python
+    run("bower install")  #js
+
+@task
+def build(prod=False):
+    cmd = "grunt " + "prod" if prod else ""
+    run(cmd)
+
 @task
 def clean():
-	if raw_input("Are you sure to run clean, you will lose any file not added to git? y/[n]") == "y":
-		run("git clean -xfd")
-
-
-@task
-def install_libs():
-	run("pip install -r requirements.txt --upgrade --target ./libx")  #python
-	run("bower install")  #js
+    if raw_input("Are you sure to run clean, you will lose any file not added to git? y/[n]") == "y":
+        run("git clean -xfd")
 
 @task
-def install_grunt():
-	run("npm i")
-
-@task
-def install_deps_ubuntu():
-	run("curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -; sudo apt-get install -y nodejs ") #nodejs
-	install_grunt()
-	run("sudo npm i -g bower")
-
-
-@task(default=True)
-def build():
-	run("grunt")
-
-
-@task
-def build_prod():
-	run("grunt prod")
-
-@task(build)
 def start(gae_path="../google_appengine/"):
     run(gae_path + "dev_appserver.py ./ --port 8090")
 
 
-@task(build_prod)
-def start_prod(gae_path="../google_appengine/"):
-    run(gae_path + "dev_appserver.py ./ --port 8090")
-
-
-
-@task(build_prod)
+@task
 def deploy(gae_path="../google_appengine/"):
 	run(gae_path + "appcfg.py update ./")
 
 
 @task
-def pylint():
-	run("pylint --ignore=lib ./* -f colorized | more")
-
-@task
-def babel_extract():
-	run("pybabel extract -F ./babel.cfg -o ./locale/messages.pot ./")
-
-@task
-def babel_init():
-	lang = raw_input()
-	run("pybabel init -l %s -d ./locale -i ./locale/messages.pot" % lang)
-
-@task
-def babel_compile():
-	run("pybabel compile -f -d ./locale")
-
-@task
-def babel_update():
-	lang = raw_input()
-	run("pybabel update -l %s -d ./locale -i ./locale/messages.pot" % lang)
+def babel(extract=False, init =False, compile=False, update=False):
+    if init:
+        lang = raw_input("language to initialize?")
+        run("pybabel init -l %s -d ./locale -i ./locale/messages.pot" % lang)
+    if extract:
+        run("pybabel extract -F ./babel.cfg -o ./locale/messages.pot ./")
+    if compile:
+        run("pybabel compile -f -d ./locale")
+    if update:
+        lang = raw_input("language to update?")
+        run("pybabel update -l %s -d ./locale -i ./locale/messages.pot" % lang)
